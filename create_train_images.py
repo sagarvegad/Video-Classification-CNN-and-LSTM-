cafc009e2c7931483fdc5141796b8b73f32263f4
Sagar Vegad <sagarfiitjee16@gmail.com>
2017-07-05 02:29:43 -0400

    Update create_train_images.py

--- create_train_images.py
+++ create_train_images.py
@@ -5,7 +5,7 @@ import cv2
 from scipy.misc import imread,imresize
 import pickle
 
-parent = os.listdir("/Users/svdj16/Documents/6_sem_mini_project/video/images")
+parent = os.listdir("/Users/.../video/images")
 
 x = []
 y = []
@@ -14,26 +14,26 @@ output = 0
 
 for video_class in parent[1:]:
     print video_class
-    child = os.listdir("/Users/svdj16/Documents/6_sem_mini_project/video/images" + "/" + video_class)
+    child = os.listdir("/Users/.../video/images" + "/" + video_class)
     for class_i in child[1:]:
-        sub_child = os.listdir("/Users/svdj16/Documents/6_sem_mini_project/video/images" + "/" + video_class + "/" + class_i)
+        sub_child = os.listdir("/Users/.../video/images" + "/" + video_class + "/" + class_i)
         for image_fol in sub_child[1:]:
             if (video_class ==  'class_4' ):
                 if(count%4 == 0):
-                    image = imread("/Users/svdj16/Documents/6_sem_mini_project/video/images" + "/" + video_class + "/" + class_i + "/" + image_fol)
+                    image = imread("/Users/.../video/images" + "/" + video_class + "/" + class_i + "/" + image_fol)
                     image = imresize(image , (224,224))
                     x.append(image)
                     y.append(output)
-                    cv2.imwrite('/Users/svdj16/Documents/6_sem_mini_project/video/' + video_class + '/' + str(count) + '_' + image_fol,image)
+                    cv2.imwrite('/Users/.../video/' + video_class + '/' + str(count) + '_' + image_fol,image)
                 count+=1
             
             else:
                 if(count%8 == 0):
-                    image = imread("/Users/svdj16/Documents/6_sem_mini_project/video/images" + "/" + video_class + "/" + class_i + "/" + image_fol)
+                    image = imread("/Users/.../video/images" + "/" + video_class + "/" + class_i + "/" + image_fol)
                     image = imresize(image , (224,224))
                     x.append(image)
                     y.append(output)
-                    cv2.imwrite('/Users/svdj16/Documents/6_sem_mini_project/video/' + video_class + '/' + str(count) + '_' + image_fol,image)
+                    cv2.imwrite('/Users/.../video/' + video_class + '/' + str(count) + '_' + image_fol,image)
                 count+=1
     output+=1
 x = np.array(x)
